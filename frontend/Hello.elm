63ac1cbf0232c6cd8a9351871236a87b54a72ecb
maduma <maduma@pt.lu>
2017-06-10 02:40:06 +0200

    going forward

--- frontend/Hello.elm
+++ frontend/Hello.elm
@@ -2,30 +2,67 @@ module Hello exposing (..)
 
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 
 
 main : Html msg
-main =
-    div []
+main = panel
+
+
+panelStyle : Attribute msg
+panelStyle =
+    style
+      [ ("width", "50%")
+      , ("margin-top", "100px")
+      , ("margin-left", "auto")
+      , ("margin-right", "auto")
+      ]
+    
+panel : Html msg
+panel =    
+    div [ panelStyle ]
         [ header
-        , list
+        , controlPanel
         , footer
         ]
+
+headerStyle : Attribute msg
+headerStyle =
+    style
+      [ ("border", "1px solid black")
+      , ("padding", "2px")
+      ]
 
         
 header : Html msg
 header =
-    div []
-        [ text "HEADER" ]
+    div [ headerStyle ]
+        [ div [ style [ ("font-size", "200%") ] ] [ text "TITLE" ]
+        , identity
+        ]
 
 
-list : Html msg        
-list =
-    div []
-        [ text "LIST"]        
+identity : Html msg        
+identity =
+    div [ style [ ("text-align", "right") ] ]
+        [ span [ style [ ("margin-right", "1%") ] ] [ text "INDETITY" ]
+        , text "LOGOUT"
+        ]
+    
+controlPanelStyle : Attribute msg
+controlPanelStyle =
+    style [ ("border", "1px solid blue")
+          , ("margin-top", "2%")
+          , ("margin-bottom", "2%")
+          ]
+
+controlPanel : Html msg        
+controlPanel =
+    div [ controlPanelStyle ]
+        [ text "CONTROL PANEL"]        
 
                 
 footer : Html msg
 footer =
-    div []
+    div [ style [ ("border", "1px solid blue") ] ]
         [ text "FOOTER" ]
